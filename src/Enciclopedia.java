b55cea665f6a52b3e57f9b2cb04039adc8694f9c
nicoledbianchin <nicoledbianchin@gmail.com>
2020-03-02 15:57:59 -0300

    molde enciclopedia

--- src/Enciclopedia.java
+++ src/Enciclopedia.java
@@ -1,2 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Enciclopedia {
+
+    private List<Livro> livros = new ArrayList<>();
+
+    public Enciclopedia() {
+    }
+
+    public String tituloDoEnesimoLivro(int posicao) {
+        return null;
+    }
+
+    public List<Livro> livrosDisponiveisEmPdf() {
+        return null;
+    }
+
+    public List<Livro> buscaPorAutora(String nomeCompleto) {
+        return null;
+    }
+
+    public List<Livro> buscaPorAnoDeLancamento(int ano) {
+        return null;
+    }
+
+    public List<Livro> buscaPorPeriodoDeLancamento(int inicio, int fim) {
+        return null;
+    }
 }
