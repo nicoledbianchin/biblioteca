4295c0728facff78b4f0c2b3bfcce3a8f2fe448d
nicoledbianchin <nicoledbianchin@gmail.com>
2020-03-02 15:49:31 -0300

    autor

--- src/Autor.java
+++ src/Autor.java
@@ -1,2 +1,33 @@
+import java.util.Objects;
+
 public class Autor {
+    private String nome;
+    private String sobrenome;
+
+    public Autor(String nome, String sobrenome) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public String toString() {
+        return nome + " " + sobrenome;
+    }
+
+    public boolean equals(Object externo) {
+        Autor outro = (Autor) externo;
+
+        if (outro == null) {
+            return false;
+        }
+
+        return Objects.equals(nome, outro.nome) && Objects.equals(sobrenome, outro.sobrenome);
+    }
 }
