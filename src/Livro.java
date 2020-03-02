0a5c63ec19ee2cbdd1b751add3b8f5d507d1e753
nicoledbianchin <nicoledbianchin@gmail.com>
2020-03-02 15:47:43 -0300

    getters e setters

--- src/Livro.java
+++ src/Livro.java
@@ -1,11 +1,37 @@
 import java.util.ArrayList;
 
 public class Livro {
-    ArrayList<Formato> formatos = new ArrayList<>();
-    ArrayList<Autor> autores = new ArrayList<>();
+    private ArrayList<Formato> formatos = new ArrayList<>();
+    private ArrayList<Autor> autores = new ArrayList<>();
+    int anoLancamento;
+    String titulo;
 
     Livro (ArrayList<Formato> formatos, ArrayList<Autor> autores){
         this.formatos = formatos;
         this.autores = autores;
+    }
+
+    public ArrayList<Formato> getFormatos() {
+        return formatos;
+    }
+
+    public ArrayList<Autor> getAutores() {
+        return autores;
+    }
+
+    public int getAnoLancamento() {
+        return anoLancamento;
+    }
+
+    public void setAnoLancamento(int anoLancamento) {
+        this.anoLancamento = anoLancamento;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
     }
 }
